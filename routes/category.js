69123afd55d851c9e450704ec6113a1a8e819c2c
Brice LESNE <brice@MacBook-Pro-de-Brice.local>
2019-10-25 14:25:40 +0200

    first commit

---
+++ routes/category.js
@@ -0,0 +1,82 @@
+const express = require("express");
+const router = express.Router();
+
+const Product = require("../models/Product");
+const Department = require("../models/Department");
+const Category = require("../models/Category");
+const User = require("../models/User");
+const Review = require("../models/Review");
+
+// READ ###############################################################
+router.get("/category", async (req, res) => {
+  let allCat = await Category.find().populate("department");
+  res.json(allCat);
+});
+
+// CREATE ###############################################################
+router.post("/category/create", async (req, res) => {
+  let title = req.body.title;
+  let description = req.body.description;
+  let departmentId = req.body.department;
+  try {
+    const newCategory = new Category({
+      title: title,
+      description: description,
+      department: departmentId
+    });
+
+    await newCategory.save();
+    res.send("Category Created");
+  } catch (error) {
+    console.log(error.message);
+    res.send(error.message);
+  }
+});
+
+// UPDATE ###############################################################
+router.post("/category/update", async (req, res) => {
+  let id = req.query.id;
+  let title = req.body.title;
+  let description = req.body.description;
+  let departmentId = req.body.department;
+  let catToUpdate = await Category.findById(id);
+  if (title) {
+    catToUpdate.title = title;
+  }
+  if (description) {
+    catToUpdate.description = description;
+  }
+  if (departmentId) {
+    catToUpdate.department = departmentId;
+  }
+  await catToUpdate.save();
+  res.json(catToUpdate);
+});
+
+// DELETE ###############################################################
+
+router.post("/category/delete", async (req, res) => {
+  let id = req.query.id;
+  let catToDelete = await Category.findById(id);
+
+  if (catToDelete) {
+    // suppression de la categorie
+    catToDelete.remove();
+
+    // supprimer aussi les produits
+    const products = await Product.find({
+      category: req.query.id
+    });
+    for (let i = 0; i < products.length; i++) {
+      await products[i].remove();
+    }
+
+    res.json({ message: "Category removed" });
+  } else {
+    res.status(400).json({
+      message: "Category not found"
+    });
+  }
+});
+
+module.exports = router;
---
+++ routes/department.js
@@ -0,0 +1,76 @@
+const express = require("express");
+const router = express.Router();
+
+const Product = require("../models/Product");
+const Department = require("../models/Department");
+const Category = require("../models/Category");
+const User = require("../models/User");
+const Review = require("../models/Review");
+
+// READ ###############################################################
+
+router.get("/department", async (req, res) => {
+  let allDept = await Department.find();
+  res.json(allDept);
+});
+
+// CREATE ###############################################################
+
+router.post("/department/create", async (req, res) => {
+  let title = req.body.title;
+  try {
+    const newDepartment = new Department({
+      title: title
+    });
+    await newDepartment.save();
+    res.send("Department Created");
+  } catch (error) {
+    console.log(error.message);
+    res.send(error.message);
+  }
+});
+
+// UPDATE ###############################################################
+
+router.post("/department/update", async (req, res) => {
+  let id = req.query.id;
+  let title = req.body.title;
+  let deptToUpdate = await Department.findById(id);
+  deptToUpdate.title = title;
+  await deptToUpdate.save();
+  res.json(deptToUpdate);
+});
+
+// DELETE ###############################################################
+
+router.post("/department/delete", async (req, res) => {
+  let id = req.query.id;
+  let deptToDelete = await Department.findById(id);
+
+  if (deptToDelete) {
+    // suppression du department
+    deptToDelete.remove();
+    // supprimer aussi les categories
+    const categories = await Category.find({
+      department: req.query.id
+    });
+    for (let i = 0; i < categories.length; i++) {
+      const products = await Product.find({
+        category: categories[i]._id
+      });
+      // supprimer aussi les produits
+      for (let j = 0; j < products.length; j++) {
+        await products[j].remove();
+      }
+      await categories[i].remove();
+    }
+
+    res.json({ message: "Department removed" });
+  } else {
+    res.status(400).json({
+      message: "Department not found"
+    });
+  }
+});
+
+module.exports = router;
